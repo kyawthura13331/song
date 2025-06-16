4c9123a232f8301176066a4394bd7a65642f63bc
kyawthurae <tyyharu245e.com>
2025-06-16 15:52:23 +0630

    webKTY

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <div className='h-full bg-blue-400 text-center'>
+      kyawthura
+
+    </div>
+    <div class="flex justify-start"> 
+      <div className='h-50 ... w-1/2 text-8xl text-cyan-950 bg-cyan-950 hover:bg-amber-600 text-center'>Lee</div>
+      <div className='h-50 ... w-1/2 text-8xl text-indigo-200 bg-indigo-200 hover:bg-cyan-900 text-center'>Bel</div>
+    </div>
+    <div class='text-center mt-6 flex justify-between text-5xl mb-5'>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>I</button>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>Love</button>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>You</button>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>So</button>
+
+    </div>
+    <div class='text-2xl ml-20'>
+    <div class="w-fit max-w-9/10 ... bg-black mb-2  hover:bg-sky-700 hover:text-white ...">I'm gonna pack my things and leave you behind,This feeling old .</div>
+    <div class="w-fit max-w-3/4 ...  bg-black mb-2  hover:bg-sky-700  hover:text-white ...">And I know that I've made up my soul </div>
+    <div class="w-fit max-w-2/3 ...  bg-black mb-2  hover:bg-sky-700  hover:text-white ...">I hope you feel what I felt when you shatterend my soul</div>
+    <div class="w-fit max-w-3/5 ...  bg-black mb-2  hover:bg-sky-700 hover:text-white ...">'Cause you wew cruel and I'm fool, so  please let me go</div>
+    </div>
+     <div class='text-center mt-6 flex justify-between text-5xl mb-5'>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>I</button>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>Love</button>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>You</button>
+      <button className='bg-black hover:bg-sky-700 ... p-5 mr-3 w-full'>So........</button>
+
+    </div>
+    </div>
+    
+  )
+}
+
+export default App
